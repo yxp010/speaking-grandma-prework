10c36b6f7c78d69d87e7c07bfa38b2c5ecef2bb3
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-28 22:52:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(words)
   if words == "I LOVE YOU GRANDMA!"
     'I LOVE YOU TOO PUMPKIN!'
-  elsif words == words.uppercase
+  elsif words == words.upcase
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
